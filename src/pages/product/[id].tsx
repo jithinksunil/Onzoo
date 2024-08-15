788ec18403e197394092a41d93cc292b59d17d41
jithinksunil <jithinksunil96@gmail.com>
2024-08-15 19:26:34 +0530

    og config done

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { ProductDetails } from '@/components/productDetails';
+import Head from 'next/head';
 export type Product = {
   id: number;
   title: string;
@@ -33,28 +34,48 @@ export default function Home() {
     if (id) fetchProduct(id as unknown as number);
   }, [id]);
   return (
-    <div className='flex flex-col min-h-screen'>
-      <div className='border-b border-gray-600 h-[78px]'>
-        <div className='max-w-[1240px] px-5 mx-auto py-4'>
-          <Image
-            src={
-              'https://oonzoo.ae/wp-content/uploads/2023/01/OONZOO-LOGO-1024x309.png'
-            }
-            width={150}
-            height={10}
-            alt='Onzoo'
-            className='min-h'
-          />
+    <>
+      <Head>
+        <title>{product?.title}</title>
+        <link
+          rel='shortcut icon'
+          href={
+            'https://oonzoo.ae/wp-content/uploads/2023/01/OONZOO-LOGO-1024x309.png'
+          }
+          type='image/x-icon'
+        />
+        <meta name='description' content={product?.description} />
+        <meta name='og:title' content={product?.title} />
+        <meta name='og:description' content={product?.description} />
+        <meta name='og:image' content={product?.image} />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:image:width' content='250' />
+        <meta property='og:image:height' content='141' />
+      </Head>
+      <div className='flex flex-col min-h-screen'>
+        <div className='border-b border-gray-600 h-[78px]'>
+          <div className='max-w-[1240px] px-5 mx-auto py-4'>
+            <Image
+              src={
+                'https://oonzoo.ae/wp-content/uploads/2023/01/OONZOO-LOGO-1024x309.png'
+              }
+              width={150}
+              height={10}
+              alt='Onzoo'
+              className='min-h'
+            />
+          </div>
+        </div>
+        <div className='flex-grow'>
+          <div className='max-w-[1220px] px-5 mx-auto py-16'>
+            <ProductDetails product={product} isLoading={loading} />
+          </div>
+        </div>
+        <div className='border-t border-gray-600'>
+          <div className='max-w-[1240px] px-5 mx-auto  py-4'>Footer</div>
         </div>
       </div>
-      <div className='flex-grow'>
-        <div className='max-w-[1220px] px-5 mx-auto py-16'>
-          <ProductDetails product={product} isLoading={loading} />
-        </div>
-      </div>
-      <div className='border-t border-gray-600'>
-        <div className='max-w-[1240px] px-5 mx-auto  py-4'>Footer</div>
-      </div>
-    </div>
+    </>
   );
 }
